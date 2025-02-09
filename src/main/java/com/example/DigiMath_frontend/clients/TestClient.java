36a88b3d13124e68b9f2c93f904fa40a37571cab
Eri1905 <ery.mtl1905@gmail.com>
2025-02-09 18:09:44 +0200

    Creating new test and showing all tests done(except templates)

---
+++ src/main/java/com/example/DigiMath_frontend/clients/TestClient.java
@@ -0,0 +1,22 @@
+package com.example.DigiMath_frontend.clients;
+
+import com.example.DigiMath_frontend.dtos.TestDTO;
+import com.example.DigiMath_frontend.dtos.UserDTO;
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.List;
+
+@FeignClient(name = "digiMath-test", url = "${backend.base-url}/test")
+public interface TestClient {
+    @GetMapping
+    List<TestDTO> getAllTests(@RequestHeader("Authorization") String auth);
+
+    @PostMapping("/create")
+    TestDTO createTest(@Valid @RequestBody TestDTO testDTO, @RequestHeader("Authorization") String auth);
+
+}
